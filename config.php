5b6519d5fdaa3a2b7b6f04940d9e11cad0e67369
dvir-pc <dvir.levy@memtech.co.il>
2017-12-20 20:38:32 +0200

    edit config for example

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$dir = "en-us"; // directory to scan. Place dir in root of script.
+$dir = "example"; // directory to scan. Place dir in root of script.
 $filecounter = 0;
 $echoHTML = false; // debug messages
